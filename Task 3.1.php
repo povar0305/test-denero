f39bc0370fcf0d56eefcff21e2a4c6b4285f6e8e
povar0305 <soficko.sofi@yandex.ru>
2021-08-31 22:31:29 +0300

    update task 3.1

--- Task 3.1.php
+++ Task 3.1.php
@@ -1,3 +1,26 @@
 <?php 
+/**<созаем класс item, который не наследуется>*/
+final class item{
 
+      /** <создаем публичный конструктор, в который передаем id элемента> */
+    public function __construct($id)
+  {
+    $this -> id = $id;
+  }
+  /** <создаем защищенные свойства> */
+  protected $id;
+  protected $name;
+  protected $status;
+  protected $changed;
+
+  /** <создаем защищенный метод init, который записывает значение в свойства элемента > */
+  protected static function init() {
+    $this -> name = $name;//записываем данные в свойства
+    $this -> status = $status; 
+  }
+    /** <создаем публичный метод save > */
+  public function save(){
+    
+  }
+}
 ?>
